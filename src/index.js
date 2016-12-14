e59436aab98c5483899e22cfe07371c0f89a95b7
Karolina Hetmanska <khetmanska30@wp.pl>
2016-12-14 22:52:32 +0100

    FDESD-87 preparing directory and files for listing places on page (PlacesView, PlaceView), added Route in src/index,js

--- src/index.js
+++ src/index.js
@@ -1,9 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import {PlacesView} from '/PlacesView'
+import {PlaceView} from '/PlaceView'
 import './index.css';
+
 
 ReactDOM.render(
   <App />,
+
+
+  <Route path="/places" component="{PlacesView}">
+    <Route path="/places/:placeId" component="{PlaceView}"/>
+  </Route>,
+
   document.getElementById('root')
-);
+)
