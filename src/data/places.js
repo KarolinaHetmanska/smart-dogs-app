5508339ca25899efd250f076c5c716b7ec266d0d
Karolina Hetmanska <khetmanska30@wp.pl>
2016-12-14 16:58:05 +0100

    places filled

--- src/data/places.js
+++ src/data/places.js
@@ -20,13 +20,68 @@ export default [
     ]
   },
   {
-    id: 1,
+    id: 2,
     name: 'Kontrast',
-    address: 'Na Plazy w Gdyni 3',
+    address: 'Bulwar Nadmorski im.Feliksa Nowowiejskiego',
     image: 'url',
     city: 'Gdynia',
-    lat: 54.3519925,
-    lng: 18.6491697,
-    events: [4]
+    lat: 54.5100893,
+    lng: 18.5528934,
+    events: []
+  },
+
+  {
+    id: 3,
+    name: 'Klub Atelier',
+    address: 'Aleja Franciszka Mamuszki 2',
+    image: 'url',
+    city: 'Sopot',
+    lat: 54.4482131,
+    lng: 18.5684754,
+    events: []
+  },
+
+  {
+    id: 4,
+    name: 'Europejskie Centrum Solidarności',
+    address: 'Plac Solidarności 1',
+    image: 'url',
+    city: 'Gdańsk',
+    lat: 54.3609089,
+    lng: 18.6489728,
+    events: []
+  },
+
+  {
+    id: 5,
+    name: 'Plac na Targu Węglowym',
+    address: 'Plac Solidarności 1',
+    image: 'url',
+    city: 'Gdańsk',
+    lat: 54.3510165,
+    lng: 18.6479549,
+    events: []
+  },
+
+  {
+    id: 6,
+    name: 'Polska Filharmonia Bałtycka',
+    address: 'Ołowianka 1',
+    image: 'url',
+    city: 'Gdańsk',
+    lat: 54.3524,
+    lng: 18.6596799,
+    events: []
+  },
+
+  {
+    id: 7,
+    name: 'Polska Filharmonia Bałtycka',
+    address: 'Ołowianka 1',
+    image: 'url',
+    city: 'Gdańsk',
+    lat: 54.3524,
+    lng: 18.6596799,
+    events: []
   }
 ]
