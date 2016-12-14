861ada22aaca307c9ce9d77aba5649391cbe8e88
KubaTuscher <Kuba Tuscher>
2016-12-14 16:09:10 +0100

    data objects added

---
+++ src/data/events.js
@@ -0,0 +1,19 @@
+export default [
+  {
+    id: 1,
+    name: 'Metallica 30-ste urodziny',
+    image: 'url',
+    date: '2016-12-24',
+    category: 'koncert',
+    description: 'Metallica juz gra 30 lat. Sa zajebisci juz od 30 lat!!!',
+  },
+  {
+    id: 2,
+    name: 'Metallica 30th anniversary',
+    image: 'url',
+    date: '2016-12-24',
+    category: 'concert',
+    description: 'An amazing concert of Metallica. Yeah, they rock already 30 years!!!',
+    place: 'Teatr Szekspirowski'
+  }
+]
---
+++ src/data/places.js
@@ -0,0 +1,32 @@
+export default [
+  {
+    id: 1,
+    name: 'Teatr Szekspirowski',
+    address: 'Wojciecha Bogusławskiego 1',
+    image: 'url',
+    city: 'Gdansk',
+    lat: 54.3519925,
+    lng: 18.6491697,
+    events: [
+      {
+        id: 1,
+        name: 'Metallica 30-ste urodziny',
+        image: 'url',
+        date: '2016-12-24',
+        category: 'koncert',
+        description: 'Metallica juz gra 30 lat. Sa zajebisci juz od 30 lat!!!',
+      },
+      3,7,9
+    ]
+  },
+  {
+    id: 1,
+    name: 'Kontrast',
+    address: 'Na Plazy w Gdyni 3',
+    image: 'url',
+    city: 'Gdynia',
+    lat: 54.3519925,
+    lng: 18.6491697,
+    events: [4]
+  }
+]
