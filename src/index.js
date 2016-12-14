59b1aed2014e8bc8cd364ef13c9ed9aabf8e4b19
KubaTuscher <Kuba Tuscher>
2016-12-14 15:38:32 +0100

    structure

--- src/index.js
+++ src/index.js
@@ -1,9 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import { Router, Route, IndexRoute, browserHistory } from 'react-router'
 import './index.css';
 
+import { App } from './App';
+import { NotFoundView } from './NotFoundView';
+
+
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap/dist/css/bootstrap-theme.css'
+
+
 ReactDOM.render(
-  <App />,
+  <Router history={browserHistory}>
+    <Route path="/" component={App}>
+
+    </Route>
+    <Route path="*" component={NotFoundView} />
+  </Router>,
   document.getElementById('root')
 );
