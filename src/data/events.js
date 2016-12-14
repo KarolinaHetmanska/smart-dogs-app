b5a849a973a867dee6387140f273f414099a291f
pawel ziezio <pawel.ziezio@yahoo.com>
2016-12-14 16:57:15 +0100

    events-PZ-WIP

--- src/data/events.js
+++ src/data/events.js
@@ -1,19 +1,78 @@
 export default [
   {
     id: 1,
-    name: 'Metallica 30-ste urodziny',
+    name: 'Kayah',
     image: 'url',
-    date: '2016-12-24',
+    date: '2017-01-11',
     category: 'koncert',
-    description: 'Metallica juz gra 30 lat. Sa zajebisci juz od 30 lat!!!',
+    hour: 19,
+    duration: 120,
+    price: 70,
+    description: 'Świąteczno-noworoczna trasa koncertowa Kayah, ' +
+              'to chęć przeniesienia najnowszej płyty artystki ' +
+                '"Gdy pada śnieg" na deski scen w Polsce i jednocześnie ' +
+                'chęć spotkania się z fanami w ten cudowny zimowy ' +
+                'okołoświąteczny okres. !',
   },
   {
     id: 2,
-    name: 'Metallica 30th anniversary',
+    name: 'Piotruś Pan ',
     image: 'url',
-    date: '2016-12-24',
-    category: 'concert',
-    description: 'An amazing concert of Metallica. Yeah, they rock already 30 years!!!',
-    place: 'Teatr Szekspirowski'
-  }
+    date: '2017-01-11',
+    category: 'musical',
+    hour: 11,
+    duration: 135,
+    price: 65,
+    description: 'Prapremiera światowa "Piotrusia Pana" w najnowocześniejszej ' +
+                  'technologii 3D na Dużej Scenie Teatru Muzycznego w Gdyni. Reżyserem gdyńskiego ' +
+                  '"Piotrusia Pana" będzie Janusz Józefowicz, aktor, reżyser i choreograf, dyrektor ' +
+                  'Teatru Studio Buffo w Warszawie, pomysłodawca i reżyser słynnego musicalu "Metro".'
+
+  },
+  {
+    id: 3,
+    name: 'Gdańskie Biennale Sztuki ',
+    image: 'url',
+    date: '2017-01-12',
+    category: 'spektakl',
+    hour: 19,
+    duration: 75,
+    price: 0,
+    description: 'Gdańskie Biennale Sztuki 2016 to czwarta edycja wydarzenia artystycznego,' +
+                 ' które zaprezentuje twórczość kilkudziesięciu artystów profesjonalnych mieszkających ' +
+                  'i tworzących w województwie pomorskim. Konkurs i wystawa, oprócz ożywienia i integracji' +
+                  ' środowiska artystycznego mają w założeniu stworzenie ciekawej propozycji kulturalnej dla' +
+                  ' mieszkańców regionu gdańskiego i turystów.'
+  },
+  {
+    id: 4,
+    name: 'Smerfy ',
+    image: 'url',
+    date: '2017-01-21',
+    category: 'spektakl',
+    hour: 16,
+    duration: 115,
+    price: 65,
+    description: 'Ogromna produkcja, której wartość przekroczyła 4 mln euro, bajkowa scenografia,' +
+    ' gra świateł, kolorowe kostiumy, ciekawe przygody z dużą dawką humoru, piękna muzyka oraz' +
+    ' interaktywne zabawy z publicznością, a przede wszystkim Wasze ulubione Smerfy razem na ' +
+    'jednej scenie! Show, które obejrzało już ponad 5 milionów widzów na świecie, gościło już ' +
+    'w największych metropoliach.'
+  },
+  {
+    id: 5,
+    name: 'Jose Gonzale ',
+    image: 'url',
+    date: '2017-02-01',
+    category: 'koncert',
+    hour: 20,
+    duration: 105
+    price: 90,
+    description: 'osé González uchodzi za skromnego, a jednocześnie pełnego twórczej ' +
+    'pasji artystę. W swojej twórczości oddaje hołd takim twórcom jak Nina Simone,' +
+    ' Chet Baker, Nick Drake, Caetano Veloso czy Cat Power. Ma krystalicznie czysty ' +
+    'głos, śpiewa melancholijne melodie, a głębokie akordy jego gitary brzmią jak u' +
+    ' argentyńskich mistrzów. Nie bez powodu uważany jest za jednego z największych ' +
+    'talentów muzycznych XXI wieku.' ,
+    }
 ]
