1c1f3c99fa54490105b4376678e83fda8cc1cd04
KubaTuscher <Kuba Tuscher>
2016-12-15 16:12:08 +0100

    details of events displayed

--- src/EventView/EventView.js
+++ src/EventView/EventView.js
@@ -1,8 +1,36 @@
 import React from 'react'
+import { events } from '../data'
+
+// <h1>Event {props.params.eventId}</h1>
 
 export default (props) =>
   <div>
+
     <h2>
-      Event {props.params.eventId}
+      {
+        events.find(
+          event => event.id === parseInt(props.params.eventId)
+        ).name
+      }
     </h2>
+    <p>
+      {
+        events.filter(
+          event => event.id === parseInt(props.params.eventId)
+        ).map(
+          event =>
+            <div key={event.id}>
+              <p>{event.id}</p>
+              <p>{event.name}</p>
+              <p>{event.image}</p>
+              <p>{event.date}</p>
+              <p>{event.category}</p>
+              <p>{event.hour}</p>
+              <p>{event.duration}</p>
+              <p>{event.price}</p>
+              <p>{event.description}</p>
+            </div>
+        )
+      }
+    </p>
   </div>
