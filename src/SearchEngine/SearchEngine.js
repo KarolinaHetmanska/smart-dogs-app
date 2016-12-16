3af7b228305ca843d29643b0437f6f58ef3c4125
Karolina Hetmanska <khetmanska30@wp.pl>
2016-12-16 15:13:34 +0100

    FDESD-69 searchform added

--- src/SearchEngine/SearchEngine.js
+++ src/SearchEngine/SearchEngine.js
@@ -1,10 +1,22 @@
 import React from 'react'
 import { Link } from 'react-router'
 import { events } from '../data'
+import { FormGroup, FormControl, Col } from 'react-bootstrap'
 
-export default (props)  =>
+export default (props) =>
   <div>
+    <Col xs={6} xsOffset={3}>
     <h1>SearchEngine</h1>
+    <form>
+      <FormGroup
+        controlId="formBasicText">
+        <FormControl
+          type="text"
+          placeholder="Co chcesz zrobić?"
+        />
+      </FormGroup>
+    </form>
+      </Col>
 
     {props.children}
 
