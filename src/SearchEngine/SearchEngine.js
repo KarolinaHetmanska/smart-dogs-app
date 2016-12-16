b1af75cc3aa5521a4cfead3271ecebed4cda09e6
Karolina Hetmanska <khetmanska30@wp.pl>
2016-12-16 16:00:40 +0100

    FDESD-69 searchform is searching by name event

--- src/SearchEngine/SearchEngine.js
+++ src/SearchEngine/SearchEngine.js
@@ -1,23 +1,58 @@
 import React from 'react'
-import { Link } from 'react-router'
-import { events } from '../data'
-import { FormGroup, FormControl, Col } from 'react-bootstrap'
+import {Link} from 'react-router'
+import {events} from '../data'
+import {FormGroup, FormControl, Col, Button} from 'react-bootstrap'
 
-export default (props) =>
-  <div>
-    <Col xs={6} xsOffset={3}>
-    <h1>SearchEngine</h1>
-    <form>
-      <FormGroup
-        controlId="formBasicText">
-        <FormControl
-          type="text"
-          placeholder="Co chcesz zrobić?"
-        />
-      </FormGroup>
-    </form>
-      </Col>
 
-    {props.children}
+export default class SearchEngine extends React.Component {
+  constructor() {
+    super()
 
-  </div>
+    this.state = {
+      search: ''
+    }
+  }
+
+  render() {
+    return (
+
+      <div>
+        <Col xs={6} xsOffset={3}>
+          <h1>SearchEngine</h1>
+          <form>
+            <FormGroup
+              controlId="formBasicText">
+              <FormControl
+                type="text"
+                placeholder="Co chcesz zrobić?"
+                value={this.state.search}
+                onChange={
+                  event => this.setState({
+                    search: event.target.value
+                })
+                }
+              />
+            </FormGroup>
+            <Button type="submit">Search</Button>
+          </form>
+          <h4>{this.state.search}</h4>
+          <ul>
+            {
+              events.filter(
+                event => event.name === this.state.search
+              ).map(
+                event =>
+                  <li key={event.id}>
+                    {event.name} {event.description}
+                  </li>
+              )
+            }
+          </ul>
+
+        </Col>
+      </div>
+
+
+    )
+  }
+}
