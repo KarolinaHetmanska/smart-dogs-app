ea671562400d79e85306c5c4255b502a4c62032f
Kuba Tuscher <jakub.tuscher@gmail.com>
2016-12-19 23:38:35 +0100

    pictures edited to bootstrap thumbnails

--- src/EventsListView/EventsListView.js
+++ src/EventsListView/EventsListView.js
@@ -1,25 +1,29 @@
 import React from 'react'
-import {Col} from 'react-bootstrap'
+import {Col, Thumbnail, Button} from 'react-bootstrap'
 import {Link} from 'react-router'
 import {events} from '../data'
 import './EventsListView.css';
 
 export default (props) => {
   return (
-      <div>
-        {
-          (props.events || events).map(event =>
-            <Col xs={3} key={event.id}>
-              <div className="eventSquare">
-                <Link to={'/events/' + event.id}>
-                  <div style={{backgroundImage: 'url(' + (process.env.PUBLIC_URL + '/img/' + event.image) + ')'}}/>
-                  <img src={process.env.PUBLIC_URL + '/img/' + event.image}/>
-                </Link>
-              </div>
-            </Col>
-          )
-        }
-      </div>
+    <div>
+      {
+        (props.events || events).map(event =>
+          <Col xs={6} sm={3} key={event.id}>
+            <Link to={'/events/' + event.id}>
+              <Thumbnail bsClass="event-thumbnail" src={process.env.PUBLIC_URL + '/img/' + event.image} alt="242x200" responsive>
+                <h3>{event.name}</h3>
+                <p>Description</p>
+                <p>
+                  <Button bsStyle="primary">Cena</Button>&nbsp;
+                  <Button bsStyle="default">Data</Button>
+                </p>
+              </Thumbnail>
+            </Link>
+          </Col>
+        )
+      }
+    </div>
 
   )
 }
--- src/EventsView/EventsView.js
+++ src/EventsView/EventsView.js
@@ -1,31 +1,35 @@
 import React from 'react'
-import {Col} from 'react-bootstrap'
+import {Col, Thumbnail, Button} from 'react-bootstrap'
 import {Link} from 'react-router'
 import {events} from '../data'
 import './EventsView.css';
 
-export default (props) => {
-    return (
-        <div>
-            <h1>Events</h1>
-            <div>
-                {
-                    (props.events || events).map(event =>
-                        <Col xs={3} key={event.id}>
-                            <div className="eventSquare">
-                                <Link to={'/events/' + event.id}>
-                                    <p>{event.name}</p>
-                                    <div
-                                        style={{backgroundImage: 'url(' + (process.env.PUBLIC_URL + '/img/' + event.image) + ')'}}/>
-                                    <img role="presentation" src={process.env.PUBLIC_URL + '/img/' + event.image}/>
-                                </Link>
-                            </div>
-                        </Col>
-                    )
-                }
-            </div>
-            {props.children}
+// <div style={{backgroundImage: 'url(' + (process.env.PUBLIC_URL + '/img/' + event.image) + ')'}}/>
 
-        </div>
-    )
+export default (props) => {
+  return (
+    <div>
+      <h1>Events</h1>
+      <div>
+        {
+          (props.events || events).map(event =>
+            <Col xs={6} sm={3} key={event.id}>
+              <Link to={'/events/' + event.id}>
+                <Thumbnail bsClass="event-thumbnail" src={process.env.PUBLIC_URL + '/img/' + event.image} alt="242x200" responsive>
+                  <h3>{event.name}</h3>
+                  <p>Description</p>
+                  <p>
+                    <Button bsStyle="primary">Cena</Button>&nbsp;
+                    <Button bsStyle="default">Data</Button>
+                  </p>
+                </Thumbnail>
+              </Link>
+            </Col>
+          )
+        }
+      </div>
+      {props.children}
+
+    </div>
+  )
 }
