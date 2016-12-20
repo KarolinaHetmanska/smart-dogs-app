ebd04b27e1fbf64506b64aff3323b5f789f4ad8e
KubaTuscher <Kuba Tuscher>
2016-12-20 15:00:06 +0100

    searchEngine basic layout done + places filter

--- src/EventsView/EventsView.js
+++ src/EventsView/EventsView.js
@@ -2,6 +2,8 @@ import React from 'react'
 import {Col, Thumbnail, Button} from 'react-bootstrap'
 import {Link} from 'react-router'
 import {events} from '../data'
+import {EventsListView} from '../EventsListView'
+
 import './EventsView.css';
 
 // <div style={{backgroundImage: 'url(' + (process.env.PUBLIC_URL + '/img/' + event.image) + ')'}}/>
@@ -10,26 +12,7 @@ export default (props) => {
   return (
     <div>
       <h1>Events</h1>
-      <div>
-        {
-          (props.events || events).map(event =>
-            <Col xs={6} sm={3} key={event.id}>
-              <Link to={'/events/' + event.id}>
-                <Thumbnail bsClass="event-thumbnail" src={process.env.PUBLIC_URL + '/img/' + event.image} alt="242x200" responsive>
-                  <h3>{event.name}</h3>
-                  <p>Description</p>
-                  <p>
-                    <Button bsStyle="primary">Cena</Button>&nbsp;
-                    <Button bsStyle="default">Data</Button>
-                  </p>
-                </Thumbnail>
-              </Link>
-            </Col>
-          )
-        }
-      </div>
-      {props.children}
-
+      <EventsListView />
     </div>
   )
 }
--- src/SearchEngine/SearchEngine.js
+++ src/SearchEngine/SearchEngine.js
@@ -1,7 +1,8 @@
 import React from 'react'
 import {events, places} from '../data'
-import {FormGroup, FormControl, Col, Button, DropdownButton, MenuItem} from 'react-bootstrap'
+import {FormGroup, FormControl, Col, Button, DropdownButton, MenuItem, Row} from 'react-bootstrap'
 import {EventsListView} from '../EventsListView'
+import './SearchEngine.css'
 
 
 export default class SearchEngine extends React.Component {
@@ -24,27 +25,39 @@ export default class SearchEngine extends React.Component {
       })
     }
     this.handleDropdownCategory = (eventKey) => {
+      if (eventKey === 'wszystko') {
+        this.setState({
+          found: events
+        })
+      } else{
       this.setState({
         found: events.filter(
           event => event.category === eventKey
-        )
+        ),
+        chosenCategory: eventKey
       })
-    }
+    }}
+
     this.handleDropdownPlace = (eventKey) => {
-      if (this.eventKey === 'undefined') {
-        return
-      }
+      if (eventKey === 'Cale') {
+        this.setState({
+          found: places
+        })
+      } else{
       console.log(eventKey);
       this.setState({
         found: places.filter(
           place => place.city === eventKey
-        )
+        ),
+        chosenPlace: eventKey
       })
-    }
+    }}
 
     this.state = {
       search: '',
-      found: []
+      found: [],
+      chosenCategory: '',
+      chosenPlace: ''
     }
   }
 
@@ -52,42 +65,67 @@ export default class SearchEngine extends React.Component {
     return (
 
       <div>
-        <Col xs={6} xsOffset={3}>
-          <h1>SearchEngine</h1>
-          <form onSubmit={this.handleSubmit}>
-            <FormGroup
-              controlId="formBasicText">
-              <FormControl
-                type="text"
-                placeholder="Co chcesz zrobić?"
-                value={this.state.search}
-                onChange={
-                  event => this.setState({
-                    search: event.target.value
-                  })
-                }
-              />
-            </FormGroup>
-            <Button type="submit">Search</Button>
-          </form>
-          <DropdownButton bsStyle={'Default'} title={'Kategorie'} onSelect={this.handleDropdownCategory}>
-            <MenuItem eventKey="koncert">koncert</MenuItem>
-            <MenuItem eventKey="musical">musical</MenuItem>
-            <MenuItem eventKey="spektakl">spektakl</MenuItem>
-            <MenuItem eventKey="#">wszystko</MenuItem>
+        <Row className="searchContainer">
+          <Col xs={8} xsOffset={2}>
+            <Col xs={6} xsOffset={3}>
+              <h1>SearchEngine</h1>
+            </Col>
+            <Col xs={10}>
+              <form onSubmit={this.handleSubmit}>
+                <FormGroup
+                  controlId="formBasicText">
+                  <FormControl
+                    type="text"
+                    placeholder="Co chcesz zrobić?"
+                    value={this.state.search}
+                    onChange={
+                      event => this.setState({
+                        search: event.target.value
+                      })
+                    }
+                  />
+                </FormGroup>
+              </form>
+            </Col>
+            <Col xs={2}>
+              <Button type="submit">Search</Button>
+            </Col>
+          </Col>
 
-          </DropdownButton>
-          <DropdownButton bsStyle={'Default'} title={'Gdzie'} onSelect={this.handleDropdownPlace}>
+          <Col xs={10} xsOffset={2}>
+            <DropdownButton id="chooseCategory" bsStyle={'default'}
+                            title={this.state.chosenCategory !== '' ? this.state.chosenCategory : 'Kategorie'}
+                            onSelect={this.handleDropdownCategory}>
+              <MenuItem eventKey="koncert">koncert</MenuItem>
+              <MenuItem eventKey="musical">musical</MenuItem>
+              <MenuItem eventKey="spektakl">spektakl</MenuItem>
+              <MenuItem eventKey="wszystko">wszystko</MenuItem>
+            </DropdownButton>
 
-            <MenuItem eventKey="Gdańsk">Gdańsk</MenuItem>
-            <MenuItem eventKey="Sopot">Sopot</MenuItem>
-            <MenuItem eventKey="Gdynia">Gdynia</MenuItem>
-          </DropdownButton>
-          <ul>
-            <EventsListView events={this.state.found}/>
-          </ul>
+            <DropdownButton id="chooseCity" bsStyle={'default'}
+                            title={this.state.chosenPlace !== '' ? this.state.chosenPlace : 'Gdzie'}
+                            onSelect={this.handleDropdownPlace}>
+              <MenuItem eventKey="Gdańsk">Gdańsk</MenuItem>
+              <MenuItem eventKey="Sopot">Sopot</MenuItem>
+              <MenuItem eventKey="Gdynia">Gdynia</MenuItem>
+              <MenuItem eventKey="Cale">Całe Trójmiasto</MenuItem>
+            </DropdownButton>
 
-        </Col>
+            <DropdownButton id="chooseDate" bsStyle={'default'} title={'Kiedy'}>
+              <MenuItem eventKey="Dzisiaj">Dzisiaj</MenuItem>
+              <MenuItem eventKey="Jutro">Jutro</MenuItem>
+              <MenuItem eventKey="Weekend">Weekend</MenuItem>
+            </DropdownButton>
+
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            <ul>
+              <EventsListView events={this.state.found}/>
+            </ul>
+          </Col>
+        </Row>
       </div>
 
 
