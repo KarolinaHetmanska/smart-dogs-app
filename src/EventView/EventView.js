d347c865a420d2634ed2691ba6db0df9486b672a
Kuba Tuscher <jakub.tuscher@gmail.com>
2016-12-20 23:15:59 +0100

    single event view basic layout complete

--- src/EventView/EventView.js
+++ src/EventView/EventView.js
@@ -1,24 +1,31 @@
 import React from 'react'
 import {events} from '../data'
+import {Grid, Row, Col} from 'react-bootstrap'
 import './Eventview.css'
 
 export default (props) =>
-  <div>
+  <Grid>
     {
       events.filter(
         event => event.id === parseInt(props.params.eventId)
       ).map(
         event =>
           <div className="singleEvent-container" key={event.id}>
-            <p><img className="event-img" src={process.env.PUBLIC_URL + '/img/' + event.image}/></p>
-            <p><h1>{event.name}</h1></p>
-            <p>Data: {event.date}</p>
-            <p>Kategoria: {event.category}</p>
-            <p>Godzina: {event.hour}.00</p>
-            <p>Czas trwania: {event.duration} min</p>
-            <p>Cena: {event.price}</p>
-            <p>{event.description}</p>
+            <Row>
+              <Col sm={4} smOffset={2}>
+                <img className="event-img" src={process.env.PUBLIC_URL + '/img/' + event.image}/>
+              </Col>
+              <Col sm={4}>
+                <h1 className="event-name">{event.name}</h1>
+                <p>Data: {event.date}</p>
+                <p>Kategoria: {event.category}</p>
+                <p>Godzina: {event.hour}.00</p>
+                <p>Czas trwania: {event.duration} min</p>
+                <p>Cena: {event.price} PLN</p>
+                <p>{event.description}</p>
+              </Col>
+            </Row>
           </div>
       )
     }
-  </div>
+  </Grid>
