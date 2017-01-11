09ac4ca41061fe703c6e85648d6a92b2b7256b88
Kuba Tuscher <jakub.tuscher@gmail.com>
2017-01-11 08:07:59 +0100

    SearchEngine layout change + menubar style change

--- src/SearchEngine/SearchEngine.js
+++ src/SearchEngine/SearchEngine.js
@@ -107,67 +107,70 @@ export default class SearchEngine extends React.Component {
 
       <div>
         <Row className="searchContainer">
-          <Col xs={8} xsOffset={2}>
+          <Col>
             <form onSubmit={this.handleSubmit}>
-              <Col xs={6} xsOffset={3}>
-                <h1>Idziemy w miasto!</h1>
-                <br />
+              <Col>
+                <h1 className="caption-main">Miasto żyje!</h1>
               </Col>
-              <Col xs={10}>
+              <Col>
+                <h3 className="caption-submain">Wszystkie eventy i wydarzenia kulturalne w jednym miejscu</h3>
+                <br/>
+              </Col>
+              <Row>
+                <Col xs={8} xsOffset={2}>
+                  <div className="searchForm-wrapper">
+                    <input className="search-form"
+                           type="text"
+                           placeholder={this.state.phrase}
+                           value={this.state.search}
+                           onChange={
+                             event => this.setState({
+                               search: event.target.value
+                             })
+                           }
+                    />
+                    <button type="submit" className="search-button">Szukaj</button>
+                  </div>
+                </Col>
+              </Row>
 
-                <FormGroup
-                  controlId="formBasicText">
-                  <FormControl
-                    type="text"
-                    placeholder={this.state.phrase}
-                    value={this.state.search}
-                    onChange={
-                      event => this.setState({
-                        search: event.target.value
-                      })
-                    }
-                  />
-                </FormGroup>
-
-              </Col>
-              <Col xs={2}>
-                <Button type="submit">Szukaj</Button>
-              </Col>
             </form>
           </Col>
+          <Row>
+            <Col>
+              <DropdownButton id="chooseCategory" bsStyle={'default'}
+                              title={this.state.chosenCategory !== '' ? this.state.chosenCategory : 'Kategorie'}
+                              onSelect={this.handleDropdownCategory}>
+                <MenuItem eventKey="koncert">koncert</MenuItem>
+                <MenuItem eventKey="musical">musical</MenuItem>
+                <MenuItem eventKey="spektakl">spektakl</MenuItem>
+                <MenuItem eventKey="wszystko">wszystko</MenuItem>
+              </DropdownButton>
 
-          <Col xs={10} xsOffset={2}>
-            <DropdownButton id="chooseCategory" bsStyle={'default'}
-                            title={this.state.chosenCategory !== '' ? this.state.chosenCategory : 'Kategorie'}
-                            onSelect={this.handleDropdownCategory}>
-              <MenuItem eventKey="koncert">koncert</MenuItem>
-              <MenuItem eventKey="musical">musical</MenuItem>
-              <MenuItem eventKey="spektakl">spektakl</MenuItem>
-              <MenuItem eventKey="wszystko">wszystko</MenuItem>
-            </DropdownButton>
+              <DropdownButton id="chooseCity" bsStyle={'default'}
+                              title={this.state.chosenPlace !== '' ? this.state.chosenPlace : 'Gdzie'}
+                              onSelect={this.handleDropdownPlace}>
+                <MenuItem eventKey="Gdańsk">Gdańsk</MenuItem>
+                <MenuItem eventKey="Sopot">Sopot</MenuItem>
+                <MenuItem eventKey="Gdynia">Gdynia</MenuItem>
+                <MenuItem eventKey="Cale">Całe Trójmiasto</MenuItem>
+              </DropdownButton>
 
-            <DropdownButton id="chooseCity" bsStyle={'default'}
-                            title={this.state.chosenPlace !== '' ? this.state.chosenPlace : 'Gdzie'}
-                            onSelect={this.handleDropdownPlace}>
-              <MenuItem eventKey="Gdańsk">Gdańsk</MenuItem>
-              <MenuItem eventKey="Sopot">Sopot</MenuItem>
-              <MenuItem eventKey="Gdynia">Gdynia</MenuItem>
-              <MenuItem eventKey="Cale">Całe Trójmiasto</MenuItem>
-            </DropdownButton>
+              <DropdownButton id="chooseDate" bsStyle={'default'}
+                              title={this.state.chosenTime !== '' ? this.state.chosenTime : 'Kiedy'}
+                              onSelect={this.handleDropdownTime}>
+                <MenuItem eventKey={604800000}>Najbliższy tydzień</MenuItem>
+                <MenuItem eventKey={2592000000}>Najbliższy miesiąc</MenuItem>
+                <MenuItem eventKey={7776000000}>Najbliższy kwartał</MenuItem>
+              </DropdownButton>
 
-            <DropdownButton id="chooseDate" bsStyle={'default'}
-                            title={this.state.chosenTime !== '' ? this.state.chosenTime : 'Kiedy'}
-                            onSelect={this.handleDropdownTime}>
-              <MenuItem eventKey={604800000}>Najbliższy tydzień</MenuItem>
-              <MenuItem eventKey={2592000000}>Najbliższy miesiąc</MenuItem>
-              <MenuItem eventKey={7776000000}>Najbliższy kwartał</MenuItem>
-            </DropdownButton>
-
-          </Col>
+            </Col>
+          </Row>
         </Row>
         <Row>
           <Col>
-            <h3 className="error-message">{this.state.found.length !== 0 ? 'Najbliższe wydarzenia dla "'+ (this.state.search || this.state.chosenCategory || this.state.chosenPlace || this.chosenTime) +'"' : this.state.errorMessage}</h3>
+            <h3
+              className="error-message">{this.state.found.length !== 0 ? 'Najbliższe wydarzenia dla "' + (this.state.search || this.state.chosenCategory || this.state.chosenPlace || this.chosenTime) + '"' : this.state.errorMessage}</h3>
             <br />
             <br />
 
