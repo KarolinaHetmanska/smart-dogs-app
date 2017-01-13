a793052ed4f1dd0ea463b672681496a07e6d11d9
Karolina Hetmanska <khetmanska30@wp.pl>
2017-01-13 16:23:32 +0100

    FDESD-107 OnePlaceView (repaired)

--- src/SingleEventView/SingleEventView.js
+++ src/SingleEventView/SingleEventView.js
@@ -34,54 +34,6 @@ const SingleEventView = props => {
         props.allEvents.filter(
           event => event.id === parseInt((props.params.eventId),10)
         ).map(
-          event =>{
-            const placeOfEvent = places.find(
-              place => place.events.indexOf(parseInt((props.params.eventId),10)) !== -1
-            )
-            return (
-              <div className="singleEvent-container" key={event.id}>
-                <Row>
-                  <Col sm={4} smOffset={2}>
-                    <img className="event-img" role="presentation" src={process.env.PUBLIC_URL + '/img/events/' + event.image}/>
-                  </Col>
-                  <Col sm={4}>
-                    <button onClick={() => props.addPersonToFavorites(event.id)}>
-                      Dodaj do ulubionych
-                    </button>
-                    <br />
-                    <hr />
-                    <h1 className="event-name">{event.name}</h1>
-                    <p>Data: {event.date}</p>
-                    <p>Kategoria: {event.category}</p>
-                    <p>Godzina: {event.hour}.00</p>
-                    <p>Czas trwania: {event.duration} min</p>
-                    <p>Cena: {event.price} PLN</p>
-                    <p>Adres: {placeOfEvent.address}</p>
-                    <p>Miasto: {placeOfEvent.city}</p>
-                  </Col>
-                </Row>
-                <br />
-                <br />
-                <Row>
-                  <Col sm={6} smOffset={2}>
-                    <p>{event.description}</p>
-                  </Col>
-                </Row>
-                <br/>
-                <br/>
-                <Row>
-                  <Col sm={6} smOffset={2}>
-
-
-                    <SingleMapView placeOfEvent={placeOfEvent
-                      }/>
-                    {console.log(props.params.eventId)}
-                  </Col>
-                </Row>
-              </div>
-            )
-          },
-
           event =>
             <div className="singleEvent-container" key={event.id}>
               <Row>
@@ -121,8 +73,7 @@ const SingleEventView = props => {
               <br/>
               <Row>
                 <Col sm={6} smOffset={2}>
-                  <SingleMapView event={props.params.eventId}/>
-                  {console.log(props.params.eventId)}
+                  <SingleMapView placeOfEvent={placeOfEvent}/>
                 </Col>
               </Row>
             </div>
