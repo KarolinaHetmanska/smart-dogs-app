a10e1f35ecb57b72e9c8d8a496f6bb0b89579b4c
Karolina Hetmanska <khetmanska30@wp.pl>
2017-01-15 19:05:01 +0100

    PlacesView-layout WIP

--- src/PlacesView/PlacesView.js
+++ src/PlacesView/PlacesView.js
@@ -1,32 +1,44 @@
 import React from 'react'
-import { connect } from 'react-redux'
-import { Thumbnail } from 'react-bootstrap'
-import { Link } from 'react-router'
+import {connect} from 'react-redux'
+import {Thumbnail, Grid, Row, Col, Image} from 'react-bootstrap'
+import {Link} from 'react-router'
+import './PlacesView.css'
 
 const mapStateToProps = (state) => ({
   places: state.placesData.places
 })
 
 const placesView = (props) =>
-  <div>
-    <h1>Places view</h1>
-    <br />{props.children}
-    <div>
-      {
-        props.params.placeId === undefined ?
-          props.places.map(place =>
-            <Link to={'/places/' + place.id} key={place.id}>
-              <Thumbnail src={process.env.PUBLIC_URL + '/img/places/' + place.image} alt="242x200">
-                <h3 className="cardheader">{place.name}</h3>
-                <div>
-                  <p> {place.city} ul.{place.address} </p>
-                </div>
-              </Thumbnail>
-            </Link>
-          ) : null
-      }
-    </div>
-  </div>
+  <Grid className="places-view-container">
+    <h1>Places View</h1>
+    <br/>{props.children}
+    {
+      props.params.placeId === undefined ?
+        props.places.map(place =>
+
+          <div>
+            <Col xs={12} lg={5} className="thumbnail places-thumbnail">
+              <Link to={'/places/' + place.id}>
+                <Col xs={6} md={6}>
+                  <Thumbnail alt="171x180"
+                             src={process.env.PUBLIC_URL + '/img/places/' + place.image} responsive/>
+                </Col>
+              </Link>
+              <Col xs={6} lg={6} className="thumbnail-description-box">
+                <h3 className="thumbnail-header">{place.name}</h3>
+                <p className="thumbnail-address"><span className="glyphicon glyphicon-map-marker"/>  {place.city}, ul.{place.address}</p>
+
+              </Col>
+              <Link to={'/places/' + place.id}>
+                <span className="thumbnail-szczegóły">szczegóły</span>
+              </Link>
+
+            </Col>
+
+          </div>
+        ) : null
+    }
+  </Grid>
 
 export default connect(mapStateToProps)(placesView)
 
--- src/state/places/reducer.js
+++ src/state/places/reducer.js
@@ -15,3 +15,6 @@ export default (state = initialState, action) => {
       return state
   }
 }
+
+
+
