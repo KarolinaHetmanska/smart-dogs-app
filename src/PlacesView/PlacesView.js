e085e9f5e9ac54b713702d4fb93d669cbbf49c26
Karolina Hetmanska <khetmanska30@wp.pl>
2017-01-15 20:41:45 +0100

    FDESD-118 layout PlacesView- finished

--- src/PlacesView/PlacesView.js
+++ src/PlacesView/PlacesView.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import {connect} from 'react-redux'
-import {Thumbnail, Grid, Row, Col, Image} from 'react-bootstrap'
+import {Grid, Col} from 'react-bootstrap'
 import {Link} from 'react-router'
 import './PlacesView.css'
 
@@ -14,27 +14,24 @@ const placesView = (props) =>
     {
       props.params.placeId === undefined ?
         props.places.map(place =>
-
-            <Col xs={12} lg={5} >
-              <div className="thumbnail thumbnail-places">
-                <Col xs={6} md={6}>
-                  <Link to={'/places/' + place.id}>
+          <Col xs={12} lg={6}>
+            <div className="thumbnail thumbnail-places">
+              <Col xs={6} md={6}>
+                <Link to={'/places/' + place.id}>
                   <div className="thumbnail-image-box"
                        style={{backgroundImage: "url(" + process.env.PUBLIC_URL + '/img/places/' + place.image + ")"}}></div>
-                  </Link>
-                </Col>
-              <Col xs={6} lg={6} className="thumbnail-description-box">
+                </Link>
+              </Col>
+              <Col xs={6} lg={6}>
                 <h3 className="thumbnail-header">{place.name}</h3>
-                <p className="thumbnail-address"><span className="glyphicon glyphicon-map-marker"/>  {place.city}
+                <p className="thumbnail-address"><span className="glyphicon glyphicon-map-marker"/> {place.city}
                   <br/> ul.{place.address}</p>
-
               </Col>
               <Link to={'/places/' + place.id}>
-                <span className="thumbnail-szczegóły">szczegóły</span>
+                <span className="thumbnail-details-button">szczegóły</span>
               </Link>
-              </div>
-            </Col>
-
+            </div>
+          </Col>
         ) : null
     }
   </Grid>
