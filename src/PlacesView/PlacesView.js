07828f0ccdc6db4de3aea1992b825bcd26f2538f
Karolina Hetmanska <khetmanska30@wp.pl>
2017-01-15 20:19:39 +0100

    Places-View layout, two coluns, images responsive,  WIP

--- src/PlacesView/PlacesView.js
+++ src/PlacesView/PlacesView.js
@@ -10,32 +10,31 @@ const mapStateToProps = (state) => ({
 
 const placesView = (props) =>
   <Grid className="places-view-container">
-    <h1>Places View</h1>
     <br/>{props.children}
     {
       props.params.placeId === undefined ?
         props.places.map(place =>
 
-          <div>
-            <Col xs={12} lg={5} className="thumbnail places-thumbnail">
-              <Link to={'/places/' + place.id}>
+            <Col xs={12} lg={5} >
+              <div className="thumbnail thumbnail-places">
                 <Col xs={6} md={6}>
-                  <Thumbnail alt="171x180"
-                             src={process.env.PUBLIC_URL + '/img/places/' + place.image} responsive/>
+                  <Link to={'/places/' + place.id}>
+                  <div className="thumbnail-image-box"
+                       style={{backgroundImage: "url(" + process.env.PUBLIC_URL + '/img/places/' + place.image + ")"}}></div>
+                  </Link>
                 </Col>
-              </Link>
               <Col xs={6} lg={6} className="thumbnail-description-box">
                 <h3 className="thumbnail-header">{place.name}</h3>
-                <p className="thumbnail-address"><span className="glyphicon glyphicon-map-marker"/>  {place.city}, ul.{place.address}</p>
+                <p className="thumbnail-address"><span className="glyphicon glyphicon-map-marker"/>  {place.city}
+                  <br/> ul.{place.address}</p>
 
               </Col>
               <Link to={'/places/' + place.id}>
                 <span className="thumbnail-szczegóły">szczegóły</span>
               </Link>
-
+              </div>
             </Col>
 
-          </div>
         ) : null
     }
   </Grid>
